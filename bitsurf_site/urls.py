ed6ca39760e890e0cf5af2f297b8c47c5c1ed848
manosai <mano.eerabathini@gmail.com>
2013-09-07 01:20:43 -0400

    added urls

--- bitsurf_site/urls.py
+++ bitsurf_site/urls.py
@@ -6,6 +6,8 @@ from django.conf import settings
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$', 'bitsurf_app.views.home')
+	url(r'^$', 'bitsurf_app.views.home'), 
+	url(r'^add-user/', 'bitsurf_app.views.add_user'), 
+	url(r'^get-clients/', 'bitsurf_app.views.get_clients')
 )
 
