70952d74a79aa8f031e31288773ccc85587755f4
manosai <mano.eerabathini@gmail.com>
2013-09-07 12:14:28 -0400

    added send_payment endpoint

--- bitsurf_site/urls.py
+++ bitsurf_site/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
 	url(r'^$', 'bitsurf_app.views.home'), 
 	url(r'^login/', 'bitsurf_app.views.get_user'), 
 	url(r'^check-site/', 'bitsurf_app.views.get_clients')
+	url(r'^send-payment/', 'bitsurf_app.views.send_payment')
 )
 
