cc5ef81d7be1b6ad24e0e4d30c62eece5b6c3ee3
manosai <mano.eerabathini@gmail.com>
2013-09-07 01:26:18 -0400

    changed url endpoint

--- bitsurf_site/urls.py
+++ bitsurf_site/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 urlpatterns = patterns('',
 	url(r'^$', 'bitsurf_app.views.home'), 
-	url(r'^add-user/', 'bitsurf_app.views.add_user'), 
-	url(r'^get-clients/', 'bitsurf_app.views.get_clients')
+	url(r'^login/', 'bitsurf_app.views.add_user'), 
+	url(r'^check-site/', 'bitsurf_app.views.get_clients')
 )
 
