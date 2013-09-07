23a6fb2d0b6f905dd68bcb56671f85c482de914c
Matthew <matthew@matthew-HP-Pavilion-dm4-Notebook-PC.(none)>
2013-09-07 10:33:31 -0400

    reverting

--- bitsurf_site/bitsurf_app/views.py
+++ bitsurf_site/bitsurf_app/views.py
@@ -19,23 +19,23 @@ def home(request):
 def add_user(request):
 	if request.method == 'GET':
 		conn = aws_connect()
-		user_domain = conn.get_domain('userTable')
-		bitcoin_address = request['bitcoinAddr']
+		user_domain = conn.get_domain('user_table')
+		bitcoin_address = request['bitcoin_addr']
 		current_attrs = user_domain.get_item(bitcoin_address, consistent_read=True)
 		if  current_attrs == None:
-			attrs = {'currentBalance': 0}
+			attrs = {'current_balance': 0}
 			user_domain.put_attributes(wallet_id, attrs)
 			json_response = json.dumps(attrs)
 		else:
-			current_balance = current_attrs['currentBalance']
-			json_response = json.dumps({'currentBalance':current_balance})
+			current_balance = current_attrs['current_balance']
+			json_response = json.dumps({'current_balance':current_balance})
 		return HttpResponse(json_response)
 
 def get_clients(request): 
 	if request.method == 'GET': 
 		conn = aws_connect()
-		business_domain = conn.get_domain('businessTable')
-		query = 'select * from `businessTable`' 
+		business_domain = conn.get_domain('business_table')
+		query = 'select * from `business_table`' 
 		rs = business_domain.select(query)
 		output = []
 		for attrs in rs: 
