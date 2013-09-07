42a18a2ee4e4a6166a2f190b6c2ab8baefd4c5ff
manosai <mano.eerabathini@gmail.com>
2013-09-06 22:51:40 -0400

    added bootstrap

--- bitsurf_site/bitsurf_app/views.py
+++ bitsurf_site/bitsurf_app/views.py
@@ -4,4 +4,4 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def home(request):
-    return render_to_response('', {}, RequestContext(request))
+    return render_to_response('login.html', {}, RequestContext(request))
--- bitsurf_site/urls.py
+++ bitsurf_site/urls.py
@@ -6,15 +6,6 @@ from django.conf import settings
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'bitsurf_site.views.home', name='home'),
-    # url(r'^bitsurf_site/', include('bitsurf_site.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    (r'media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    url(r'^home/', 'bitsurf_site.bitsurf_app.views.home', name='home')
+	url(r'^$', 'bitsurf_app.views.home')
 )
+
